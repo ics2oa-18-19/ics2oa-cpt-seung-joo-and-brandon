3889af23f34908f9e99d9c2f8bab1d56ca81a907
Brandon589 <47423841+Brandon589@users.noreply.github.com>
2019-05-31 10:46:00 -0400

    Update CPT.py

--- dev/CPT.py
+++ dev/CPT.py
@@ -1,43 +1,94 @@
+import arcade
 import time
 import random
-import time
 
 high_score = 0
-
 scramble = ['L', 'L2', 'L*', 'R', 'R2', 'R*',  'D', 'D2', 'D*', 'B', 'B*', 'B2', 'F', 'F2', 'F*', 'U', 'U2', 'U*']
 
-while len(scramble) < 21:
-    random.shuffle(scramble)
+# Global variables: variable that works with everything
+WIDTH = 640
+HEIGHT = 480
 
-    print(scramble)
+# This is when you want to move the character
+def update(delta_time):
+   global high_score
+   while len(scramble) < 21:
+       random.shuffle(scramble)
 
-    start = input("Press enter to start the time")
+       print(scramble)
 
-    print("SOLVE")
+       start = input("Press enter to start the time")
 
-    begin = time.time()
+       print("SOLVE")
 
-    endtimer = input("Press enter to stop the timer")
+       begin = time.time()
 
-    end = time.time()
+       endtimer = input("Press enter to stop the timer")
 
-    elapsed = end - begin
+       end = time.time()
 
-    elapsed = float(elapsed)
+       elapsed = end - begin
 
-    elapsed = round(elapsed, 2)
+       elapsed = float(elapsed)
 
-    if high_score == 0:
-        high_score = elapsed
-        print('The high score is', high_score, "Seconds")
-    
-    if elapsed < high_score:
-        difference = high_score - elapsed
-        print('You beat your high score by', difference, "Seconds")
+       elapsed = round(elapsed, 2)
 
-        difference = (round, 2)
 
-    print("The time you took is", elapsed, "Seconds")
+       if high_score == 0:
+           high_score = elapsed
+           print('The high score is', high_score, "Seconds")
+
+       if elapsed < high_score:
+           print("You beat your high score!")
+           difference = (round, 2)
+
+       print("The time you took is", elapsed, "Seconds")
+
+       f = open("guru99.txt", "w+")
+       f.write(str(elapsed))
+       f.close()
+       print(elapsed)
+
+
+def on_draw():
+   arcade.start_render()
+   # Draw in here...
+   arcade.draw_circle_filled(100, 100, 25, arcade.color.BLUE)
+
+
+def on_key_press(key, modifiers):
+   pass
+
+
+def on_key_release(key, modifiers):
+   pass
+
+
+def on_mouse_press(x, y, button, modifiers):
+   pass
+
+def on_mouse_release(x, y, button, modifiers):
+   pass
+
+
+def setup():
+   arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
+   arcade.set_background_color(arcade.color.WHITE)
+   arcade.schedule(update, 1/60)
+
+   # Override arcade window methods
+   window = arcade.get_window()
+   window.on_draw = on_draw
+   window.on_key_press = on_key_press
+   window.on_key_release = on_key_release
+   window.on_mouse_press = on_mouse_press
+
+   arcade.run()
+
+
+if __name__ == '__main__':
+   setup()
+
     
 
 
